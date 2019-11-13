dec82c1df37108e471543c2d3e86b18ddf832438
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 05:04:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   #hash1 = color_gender_lives hash 
-  data.collect do {|hash1, value| value.collect
+  data.collect do |hash1, value|
+    value.collect do |h{|hash1, value| value.collect
   result
 end
