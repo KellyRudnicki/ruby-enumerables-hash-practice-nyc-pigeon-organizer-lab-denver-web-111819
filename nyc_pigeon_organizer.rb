7fdc61b6b344e919e4d1e549e274bacd74390f87
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2019-11-13 05:07:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,4 +6,19 @@ def nyc_pigeon_organizer(data)
     value.collect do |hash2, value2| 
       {|hash1, value| value.collect
   result
+  
+  data.collect do |key, value|
+    value.collect do |key2, value2|
+      value2.collect do |name|
+        if result[name] == nil 
+          result[name] = {}
+        end
+        if result[name][key] == nil 
+          result[name][key] = []
+        end
+        result[name][key].push(stats.to_s)
+      end
+    end 
+  end 
+  result
 end
